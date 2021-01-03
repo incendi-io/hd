3e70badee0390f22c1f4c9d9f6d8b8c036430eb8
seonetartem <seonetartem@gmail.com>
2021-01-03 12:59:56 +0200

    refactor(gatsby): move private creds to storage

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,4 @@
-require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV || 'development'}`,
-})
+require('dotenv').config()
 
 const contentfulConfig = {
   spaceId: process.env.CONTENTFUL_SPACE_ID,
@@ -9,7 +7,7 @@ const contentfulConfig = {
 
 const fireStoreConfig = {
   private_key_id: process.env.FIRESTORE_PRIVATE_KEY_ID,
-  private_key: process.env.FIRESTORE_PRIVATE_KEY
+  private_key: (process.env.FIRESTORE_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
 }
 
 if (process.env.CONTENTFUL_HOST) {
@@ -20,19 +18,18 @@ if (process.env.CONTENTFUL_HOST) {
 const { spaceId, accessToken } = contentfulConfig
 
 const creds = {
-  "type": "service_account",
-  "project_id": "hdweb-9f081",
-  "private_key_id": fireStoreConfig.private_key_id,
-  "private_key": fireStoreConfig.private_key,
-  "client_email": "firebase-adminsdk-c9kd8@hdweb-9f081.iam.gserviceaccount.com",
-  "client_id": "118062450931160324964",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-c9kd8%40hdweb-9f081.iam.gserviceaccount.com"
-};
-
-console.log(creds);
+  type: 'service_account',
+  project_id: 'hdweb-9f081',
+  private_key_id: fireStoreConfig.private_key_id,
+  private_key: fireStoreConfig.private_key,
+  client_email: 'firebase-adminsdk-c9kd8@hdweb-9f081.iam.gserviceaccount.com',
+  client_id: '118062450931160324964',
+  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+  token_uri: 'https://oauth2.googleapis.com/token',
+  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+  client_x509_cert_url:
+    'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-c9kd8%40hdweb-9f081.iam.gserviceaccount.com',
+}
 
 if (!spaceId || !accessToken) {
   throw new Error('Contentful spaceId and the access token need to be provided.')
