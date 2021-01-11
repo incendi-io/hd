9cb52f427780dceece7b40a7b0403646801ab239
Alexey Lukin <alexey.lukin@da-14.com>
2021-01-11 09:49:01 +0200

    update shop parts block

--- src/templates/Home/components/Parts/index.tsx
+++ src/templates/Home/components/Parts/index.tsx
@@ -3,34 +3,54 @@ import Container from 'react-bootstrap/cjs/Container'
 import Dropdown from 'react-bootstrap/cjs/Dropdown'
 
 const Parts: FC<unknown> = (): ReactElement => (
-  <Container fluid>
-    <Dropdown>
-      <Dropdown.Toggle id="dropdown-basic">Shop For Parts</Dropdown.Toggle>
+  <div className="row-splitter container-dark-background pb-0 pt-0">
+    <Container fluid className="container pb-0 pt-0">
+      <Dropdown>
+        <Dropdown.Toggle
+          id="dropdown-basic"
+          className="btn btn-secondary container-dark-background btn-sm dropdown-toggle py-3 pb-2 border-0">
+          <span className="h4 text-capitalize text-white pr-1">Shop For Parts</span>
+        </Dropdown.Toggle>
 
-      <Dropdown.Menu>
-        <Dropdown.Header>Buying Online</Dropdown.Header>
-        <Dropdown.Item
-          rel="noopener noreferrer"
-          target="_blank"
-          href="https://parts.cat.com/en/hastingsdeering?utm_source=hastingsdeering.com.au&amp;utm_content=home+page+visit+online+store+btn">
-          Visit Online Store
-        </Dropdown.Item>
-        <Dropdown.Item href="/parts/how-to-order-online">How To / Tutorials</Dropdown.Item>
-        <Dropdown.Header>Learn More</Dropdown.Header>
-        <Dropdown.Item href="/parts/cat-undercarriage">Undercarriage</Dropdown.Item>
-        <Dropdown.Item href="/parts/ground-engaging-tools">Ground Engaging Tools</Dropdown.Item>
-        <Dropdown.Item href="/parts/hose-and-couplings">
-          Hydraulics: Hose &amp; Couplings
-        </Dropdown.Item>
-        <Dropdown.Item href="/parts/cat-oils-fluids-filters">
-          Oils, Fluids &amp; Filters
-        </Dropdown.Item>
-        <Dropdown.Item href="/parts/cat-batteries">Batteries</Dropdown.Item>
-        <Dropdown.Item href="/owning/repair-options">Repair Options</Dropdown.Item>
-        <Dropdown.Item href="/owning/repair-options/rebuild">Certified Rebuilds</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
-  </Container>
+        <Dropdown.Menu className="position-center">
+          <Dropdown.Header className="text-primary">Buying Online</Dropdown.Header>
+          <Dropdown.Item
+            className="text-light text-secondary bg-light"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://parts.cat.com/en/hastingsdeering?utm_source=hastingsdeering.com.au&amp;utm_content=home+page+visit+online+store+btn">
+            Visit Online Store
+          </Dropdown.Item>
+          <Dropdown.Item className="text-light" href="/parts/how-to-order-online">
+            How To / Tutorials
+          </Dropdown.Item>
+
+          <Dropdown.Header className="text-primary">Learn More</Dropdown.Header>
+          <Dropdown.Item className="text-light" href="/parts/cat-undercarriage">
+            Undercarriage
+          </Dropdown.Item>
+          <Dropdown.Item className="text-light" href="/parts/ground-engaging-tools">
+            Ground Engaging Tools
+          </Dropdown.Item>
+          <Dropdown.Item className="text-light" href="/parts/hose-and-couplings">
+            Hydraulics: Hose &amp; Couplings
+          </Dropdown.Item>
+          <Dropdown.Item className="text-light" href="/parts/cat-oils-fluids-filters">
+            Oils, Fluids &amp; Filters
+          </Dropdown.Item>
+          <Dropdown.Item className="text-light" href="/parts/cat-batteries">
+            Batteries
+          </Dropdown.Item>
+          <Dropdown.Item className="text-light" href="/owning/repair-options">
+            Repair Options
+          </Dropdown.Item>
+          <Dropdown.Item className="text-light" href="/owning/repair-options/rebuild">
+            Certified Rebuilds
+          </Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    </Container>
+  </div>
 )
 
 export default Parts
