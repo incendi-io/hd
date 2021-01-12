a6ade7e7b03ebc316d448af13ad86364a65aed9f
Alexey Lukin <alexey.lukin@da-14.com>
2021-01-12 23:13:42 +0200

    fix equalized content

--- src/templates/Home/components/EqualizedContent/index.tsx
+++ src/templates/Home/components/EqualizedContent/index.tsx
@@ -3,31 +3,34 @@ import Col from 'react-bootstrap/cjs/Col'
 import Row from 'react-bootstrap/cjs/Row'
 
 import EqualizedItem from '../EqualizedItem'
+import styles from './EqualizedContent.module.scss'
 
 const EqualizedContent: FC<unknown> = (): ReactElement => (
-  <Row>
-    <Col md={4}>
-      <EqualizedItem
-        title="Servicing And Repair"
-        backgroundImage="/images/966wl-aftermarket_opt_800x533.jpg"
-        button={{ title: 'VIEW OPTIONS', link: '/owning/repair-options' }}
-      />
-    </Col>
-    <Col md={4}>
-      <EqualizedItem
-        title="Technology Solutions"
-        backgroundImage="/images/hastings-deering--technology-solution.jpg"
-        button={{ title: 'LEARN MORE', link: '/owning/technology-solutions' }}
-      />
-    </Col>
-    <Col md={4}>
-      <EqualizedItem
-        title="Technology Solutions"
-        backgroundImage="/images/careers-hastings-deering.jpg"
-        button={{ title: 'LEARN MORE', link: '/owning/technology-solutions' }}
-      />
-    </Col>
-  </Row>
+  <div className="row-splitter equalized-content mt-3 pt-0">
+    <Row className="equalized-content mt-3 pt-0 container">
+      <Col md={4} className={styles.promoWithBackground}>
+        <EqualizedItem
+          title="Servicing And Repair"
+          backgroundImage="/images/966wl-aftermarket_opt_800x533.jpg"
+          button={{ title: 'VIEW OPTIONS', link: '/owning/repair-options' }}
+        />
+      </Col>
+      <Col md={4} className={styles.promoWithBackground}>
+        <EqualizedItem
+          title="Technology Solutions"
+          backgroundImage="/images/hastings-deering--technology-solution.jpg"
+          button={{ title: 'LEARN MORE', link: '/owning/technology-solutions' }}
+        />
+      </Col>
+      <Col md={4} className={styles.promoWithBackground}>
+        <EqualizedItem
+          title="Technology Solutions"
+          backgroundImage="/images/careers-hastings-deering.jpg"
+          button={{ title: 'LEARN MORE', link: '/owning/technology-solutions' }}
+        />
+      </Col>
+    </Row>
+  </div>
 )
 
 export default EqualizedContent
--- src/templates/Home/components/EqualizedItem/index.tsx
+++ src/templates/Home/components/EqualizedItem/index.tsx
@@ -1,5 +1,8 @@
+import classNames from 'classnames'
 import React, { FC, ReactElement } from 'react'
 import Button from 'react-bootstrap/cjs/Button'
+
+import styles from './EqualizedItem.module.scss'
 
 type Props = {
   title: string
@@ -11,11 +14,33 @@ type Props = {
 }
 
 const EqualizedItem: FC<Props> = ({ title, backgroundImage, button }): ReactElement => (
-  <div style={{ backgroundImage: `url("${backgroundImage}")`, height: 380 }}>
-    <h3 className="text-white">{title}</h3>
-    <Button variant="outline-light" href={button.link}>
-      {button.title}
-    </Button>
+  <div className={styles.promoWithBackground}>
+    <div className={styles.promoBackground}>
+      <span
+        className={classNames(styles.backgroundSection, styles.backgroundOverlayGrid)}
+        style={{ backgroundImage: `url("${backgroundImage}")`, height: 380 }}
+      />
+      <span
+        className={classNames(styles.backgroundOverlayGradient, styles.backgroundOverlayGrid)}
+      />
+    </div>
+    <div className={styles.promoText}>
+      <h3 className={classNames(styles.promoHeading, 'text-white')}>{title}</h3>
+      <p className="promo-subheading hd-p1 field-promodescription">
+        <br />
+      </p>
+    </div>
+    <div className={styles.promoCtaSection}>
+      <div className={styles.fieldPromocta}>
+        <Button
+          as="a"
+          className={classNames(styles.fieldPromoctaA, 'text-white btn-lg mt-3')}
+          variant="outline-light"
+          href={button.link}>
+          {button.title}
+        </Button>
+      </div>
+    </div>
   </div>
 )
 
