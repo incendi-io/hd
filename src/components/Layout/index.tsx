3aa0822899b8bc199d4aba5dcbfd842b2d5c3149
Alexey Lukin <alexey.lukin@da-14.com>
2021-01-14 00:01:49 +0200

    add back to top button

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,7 +3,10 @@ import './index.scss'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 
+import { faChevronUp } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React, { FC } from 'react'
+import BackToTop from 'react-back-to-top'
 
 import Footer from '~components/Footer'
 import { Header } from '~components/Header'
@@ -17,6 +20,15 @@ const Layout: FC<Props> = (props): React.ReactElement => (
     <Header />
     <main>{props.children}</main>
     <Footer />
+    <BackToTop
+      mainStyle={{
+        background: 'var(--bg-grey)',
+      }}
+      offsetTop={20}
+      step={50}
+      isPercent={false}>
+      <FontAwesomeIcon icon={faChevronUp} title={'Read'} />
+    </BackToTop>
   </div>
 )
 
