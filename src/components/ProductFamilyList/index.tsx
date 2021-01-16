a7618b9c90086d806ca74bafaa60d8456473ca31
Alexey Lukin <alexey.lukin@da-14.com>
2021-01-16 22:26:18 +0200

    finish products page

--- src/components/ProductFamilyList/index.tsx
+++ src/components/ProductFamilyList/index.tsx
@@ -1,23 +1,55 @@
 //TODO: css modules not working again, wtf, how even debug it?
 import './ProductFamilyList.modules.scss'
 
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { graphql, useStaticQuery } from 'gatsby'
-import React from 'react'
-import { Col } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Button, Col } from 'react-bootstrap'
 import Row from 'react-bootstrap/Row'
+import { StringParam, useQueryParam } from 'use-query-params'
 
 import ProductFamilyCard, { FamilyProduct } from '~components/ProductFamilyCard'
 
 const ProductFamilyList = (): React.ReactElement => {
+  const [q, setQ] = useQueryParam('q', StringParam)
+  const [searchValue, setSearchValue] = useState(q as string)
+
   const data = useStaticQuery(query)
   const list = familyProductsMapper(data.familyProducts)
+  //TODO: add search to graphql query instead
+  const filteredList = list.filter((product) => product.name.match(new RegExp(q as string, 'i')))
 
   return (
     <Col className="container">
       <Row>
+        <Col xs={12} lg={6} className="component search-box horizontal product-search initialized">
+          <div className="component-content">
+            <span className="twitter-typeahead">
+              <input
+                value={searchValue}
+                type="text"
+                className="search-box-input form-control tt-hint"
+                autoComplete="off"
+                spellCheck="false"
+                placeholder="Search"
+                onChange={(e) => setSearchValue(e.target.value)}
+                onKeyPress={(e) => e.key === 'Enter' && setQ(searchValue)}
+              />
+            </span>
+            <Button
+              className="search-box-button"
+              type="submit"
+              size={'lg'}
+              onClick={() => setQ(searchValue)}>
+              <FontAwesomeIcon icon={faSearch} title={'Find'} />
+            </Button>
+          </div>
+        </Col>
         <Col xs={12} className="component search-results search-result-product-category">
+          {!filteredList.length && <div className="no-results">No results found</div>}
           <ul className="search-result-list">
-            {list.map((product) => (
+            {filteredList.map((product) => (
               <li key={product.id}>
                 <ProductFamilyCard item={product} />
               </li>
