5d990b24e2a395c22270bf60da742748cb173c70
Alexey Lukin <alexey.lukin@da-14.com>
2021-01-19 14:44:28 +0200

    fix mobile promo styles

--- src/components/PromoImage/index.tsx
+++ src/components/PromoImage/index.tsx
@@ -1,4 +1,3 @@
-import classNames from 'classnames'
 import React, { FC } from 'react'
 import { Col } from 'react-bootstrap'
 
@@ -15,19 +14,14 @@ type Props = {
 const PromoImage: FC<Props> = ({ url, title, subTitle, description, useOverlay }) => (
   <Col
     xs={12}
-    className={classNames(
-      styles.container,
-      'component promo promo-hero promo-hero-with-description'
-    )}>
-    <div className="component-content">
+    className={`${styles.container} component promo promo-hero promo-hero-with-description`}>
+    <div className="component-content position-relative">
       <div className={styles.promoBackground}>
-        <span
-          className={styles.backgroundSection}
-          style={{ backgroundImage: `url(${url})` }}></span>
-        {useOverlay && <span className={styles.overlay}></span>}
+        <span className={styles.backgroundSection} style={{ backgroundImage: `url(${url})` }} />
+        {useOverlay && <span className={styles.overlay} />}
       </div>
-      <div className="container overflow-auto">
-        <div className={classNames(styles.promoText, 'offset-md-1')}>
+      <div className={`${styles.containerSmall} container overflow-auto`}>
+        <div className={`${styles.promoText} offset-md-1`}>
           {subTitle && <h3 className={styles.promoTextH3}>{subTitle}</h3>}
           {title && <h1 className={styles.promoHeading}>{title}</h1>}
           {description && <p className={styles.promoTextP}>{description}</p>}
