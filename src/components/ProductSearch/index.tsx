4d7e2fa69724d3fbfaf8160ab65fdad550fee1c2
Alexey Lukin <alexey.lukin@da-14.com>
2021-01-27 16:06:44 +0200

    update search with searchButtonPosition and categories

--- src/components/ProductSearch/index.tsx
+++ src/components/ProductSearch/index.tsx
@@ -3,28 +3,59 @@ import './ProductSearch.scss'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React, { FC, useEffect, useState } from 'react'
-import { Button, Col } from 'react-bootstrap'
+import { Button, Col, Dropdown, DropdownButton, InputGroup } from 'react-bootstrap'
 import Row from 'react-bootstrap/Row'
 import { StringParam, useQueryParam } from 'use-query-params'
 
+type Category = {
+  title: string
+  slug: string
+}
 type Props = {
   searchValueChange?: (searchValue: string) => void
+  catValueChange?: (catValue: string) => void
   className?: string
+  fullWidth?: boolean
+  searchButtonPosition?: 'left' | 'right'
+  categories?: Category[]
 }
 
-const ProductSearch: FC<Props> = ({ className, searchValueChange }) => {
+const ProductSearch: FC<Props> = ({
+  className,
+  searchValueChange,
+  catValueChange,
+  fullWidth,
+  searchButtonPosition = 'right',
+  categories = [],
+}) => {
   const [q, setQ] = useQueryParam('q', StringParam)
   const [searchValue, setSearchValue] = useState(q as string)
-
   useEffect(() => {
     if (searchValueChange && typeof q !== 'undefined' && q !== null) searchValueChange(q)
   }, [q])
 
+  const [cat, setCat] = useQueryParam('cat', StringParam)
+  useEffect(() => {
+    if (catValueChange && typeof cat !== 'undefined' && cat !== null) catValueChange(cat)
+  }, [cat])
+
   return (
     <Col className={className}>
       <Row>
-        <Col xs={12} lg={6} className="component search-box horizontal product-search initialized">
+        <Col
+          xs={12}
+          lg={fullWidth ? 12 : 6}
+          className="component search-box horizontal product-search initialized">
           <div className="component-content">
+            {searchButtonPosition === 'left' && (
+              <Button
+                className="search-box-button"
+                type="submit"
+                size={'lg'}
+                onClick={() => setQ(searchValue)}>
+                <FontAwesomeIcon icon={faSearch} title={'Find'} />
+              </Button>
+            )}
             <span className="twitter-typeahead">
               <input
                 value={searchValue}
@@ -37,13 +68,35 @@ const ProductSearch: FC<Props> = ({ className, searchValueChange }) => {
                 onKeyPress={(e) => e.key === 'Enter' && setQ(searchValue)}
               />
             </span>
-            <Button
-              className="search-box-button"
-              type="submit"
-              size={'lg'}
-              onClick={() => setQ(searchValue)}>
-              <FontAwesomeIcon icon={faSearch} title={'Find'} />
-            </Button>
+            {categories.length > 0 && (
+              <DropdownButton
+                as={InputGroup.Append}
+                variant="outline-dark"
+                className="btn-white search-box-button-border"
+                title="Category"
+                onSelect={(slug) => {
+                  setCat(slug)
+                }}>
+                <Dropdown.Item href="all" onClick={(e) => e.preventDefault()}>
+                  All categories
+                </Dropdown.Item>
+                <Dropdown.Divider />
+                {categories.map((cat, index) => (
+                  <Dropdown.Item key={index} href={cat.slug} onClick={(e) => e.preventDefault()}>
+                    {cat.title}
+                  </Dropdown.Item>
+                ))}
+              </DropdownButton>
+            )}
+            {searchButtonPosition === 'right' && (
+              <Button
+                className="search-box-button"
+                type="submit"
+                size={'lg'}
+                onClick={() => setQ(searchValue)}>
+                <FontAwesomeIcon icon={faSearch} title={'Find'} />
+              </Button>
+            )}
           </div>
         </Col>
       </Row>
