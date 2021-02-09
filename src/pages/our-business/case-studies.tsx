7014efc283e34db3fa70e2af9584b1622fea5961
Alexey Lukin <alexey.lukin@da-14.com>
2021-02-09 02:03:03 +0200

    replace useStaticQuery with StaticQuery

--- src/pages/our-business/case-studies.tsx
+++ src/pages/our-business/case-studies.tsx
@@ -1,6 +1,6 @@
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { useStaticQuery } from 'gatsby'
+import { StaticQuery, useStaticQuery } from 'gatsby'
 import React from 'react'
 import { Button } from 'react-bootstrap'
 import Col from 'react-bootstrap/cjs/Col'
@@ -23,63 +23,76 @@ const mockCategories = [
 ]
 
 const CaseStudiesPage = (): React.ReactElement => {
-  const data = useStaticQuery(query)
-  let news = newsMapper(data.news)
-  if (!news.length) news = mockNews
-
   return (
-    <Layout>
-      <PromoImage
-        url="/images/leadership_800x533.jpg"
-        useOverlay={true}
-        title="Case Studies"
-        subTitle="OUR BUSINESS"
-      />
-      <div className="component row-splitter ">
-        <div className="container">
-          <Row>
-            <Col xs={12} className="component navigation-title initialized">
-              <Breadcrumbs
-                list={[
-                  {
-                    title: 'Our Business',
-                    url: '/our-business',
-                  },
-                  {
-                    title: 'Case Studies',
-                    url: '/our-business/case-studies',
-                  },
-                ]}
-              />
-            </Col>
-            <Col xs={12} sm={12} md={12} lg={12} className="component rich-text">
-              <div className="component-content">
-                <h1>Case Studies</h1>
+    <StaticQuery
+      query={query}
+      render={(data) => {
+        let news = newsMapper(data.news)
+        if (!news.length) news = mockNews
+
+        return (
+          <Layout>
+            <PromoImage
+              url="/images/leadership_800x533.jpg"
+              useOverlay={true}
+              title="Case Studies"
+              subTitle="OUR BUSINESS"
+            />
+            <div className="component row-splitter ">
+              <div className="container">
+                <Row>
+                  <Col xs={12} className="component navigation-title initialized">
+                    <Breadcrumbs
+                      list={[
+                        {
+                          title: 'Our Business',
+                          url: '/our-business',
+                        },
+                        {
+                          title: 'Case Studies',
+                          url: '/our-business/case-studies',
+                        },
+                      ]}
+                    />
+                  </Col>
+                  <Col xs={12} sm={12} md={12} lg={12} className="component rich-text">
+                    <div className="component-content">
+                      <h1>Case Studies</h1>
+                    </div>
+                  </Col>
+                  <Col
+                    xs={3}
+                    lg={3}
+                    className="component facet-single-selection-list facet-sxatags facet-component">
+                    <FacetSearch list={mockCategories} />
+                  </Col>
+                  <Col
+                    xs={9}
+                    lg={9}
+                    className="component search-results search-result-related-news">
+                    <StudiesList news={news} />
+                  </Col>
+                  <Col xs={12} lg={{ span: 4, offset: 4 }} className="component load-more">
+                    <div className="component-content hd-btn icon-right icon-chevron-right p-0">
+                      <Button
+                        variant={'primary'}
+                        className="btn-block hd-btn d-flex align-items-center justify-content-center">
+                        <span>Load More</span>
+                        <FontAwesomeIcon
+                          icon={faChevronRight}
+                          width={28}
+                          className={styles.iconRight}
+                        />
+                      </Button>
+                    </div>
+                  </Col>
+                </Row>
               </div>
-            </Col>
-            <Col
-              xs={3}
-              lg={3}
-              className="component facet-single-selection-list facet-sxatags facet-component">
-              <FacetSearch list={mockCategories} />
-            </Col>
-            <Col xs={9} lg={9} className="component search-results search-result-related-news">
-              <StudiesList news={news} />
-            </Col>
-            <Col xs={12} lg={{ span: 4, offset: 4 }} className="component load-more">
-              <div className="component-content hd-btn icon-right icon-chevron-right p-0">
-                <Button
-                  variant={'primary'}
-                  className="btn-block hd-btn d-flex align-items-center justify-content-center">
-                  <span>Load More</span>
-                  <FontAwesomeIcon icon={faChevronRight} width={28} className={styles.iconRight} />
-                </Button>
-              </div>
-            </Col>
-          </Row>
-        </div>
-      </div>
-    </Layout>
+            </div>
+          </Layout>
+        )
+      }}
+    />
   )
 }
 
